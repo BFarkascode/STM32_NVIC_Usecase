1cb7cfacae605cd1984e0634d6a4a11fc458debb
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2024-02-07 09:52:43 +0100

    Update main.c

--- NVIC_Usecase_Blinky/main.c
+++ NVIC_Usecase_Blinky/main.c
@@ -1,5 +1,16 @@
 /* USER CODE BEGIN Header */
 /**
+ * Project: STM32_NVIC_Usecase/NVIC_Usecase_Blinky
+ * File: main.c
+ * Author: BalazsFarkas
+ * Processor: STM32L053R8
+ * Compiler: ARM-GCC (STM32 IDE)
+ * Program version: 1.0
+ * Program description: N/A
+ * Hardware description/pin distribution: N/A
+ * Modified from: N/A
+ * Change history: N/A
+
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
